5cf71fdbc8e607587def6f56035c020220348f6b
Raisal Rasheed <raisalrasheed545@gmail.com>
2025-05-16 06:53:17 +0530

    update server

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,14 @@ require('dotenv').config();
 const app = express();
 app.use(
   cors({
-    origin: 'https://taskmanagment-il4n.onrender.com',
+    origin: 'https://task-managment-sand.vercel.app/',
   })
 );
 app.use(express.json());
 
 mongoose
   .connect(
-    'mongodb+srv://raisalrasheed545:sRDAcJ4B4CUk2O7p@taskmanagement.z4vvymx.mongodb.net/?retryWrites=true&w=majority&appName=taskManagement',
-    {}
+    'mongodb+srv://raisalrasheed545:sRDAcJ4B4CUk2O7p@taskmanagement.z4vvymx.mongodb.net/?retryWrites=true&w=majority&appName=taskManagement'
   )
   .then(() => console.log('MongoDB Connected'));
 
