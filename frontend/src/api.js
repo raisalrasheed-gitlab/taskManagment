6926c146bd3a3afc674488067a2732c94a5ba132
Raisal Rasheed <raisalrasheed545@gmail.com>
2025-05-16 07:03:59 +0530

    update server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://taskmanagment-il4n.onrender.com',
+  baseURL: 'https://taskmanagment-il4n.onrender.com/api/',
 });
 
 export const getTasks = status =>
-  API.get('api/tasks', status ? { params: { status } } : {});
-export const createTask = task => API.post('api/tasks', task);
-export const updateTask = (id, task) => API.put(`api/tasks/${id}`, task);
-export const deleteTask = id => API.delete(`api/tasks/${id}`);
+  API.get('tasks', status ? { params: { status } } : {});
+export const createTask = task => API.post('tasks', task);
+export const updateTask = (id, task) => API.put(`tasks/${id}`, task);
+export const deleteTask = id => API.delete(`tasks/${id}`);
